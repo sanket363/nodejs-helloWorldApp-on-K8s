894bf52a1fca28305118c2a23f3b8a3abf824080
sanket363@gmail.com <sanketbhalke363@gmail.com>
2024-07-05 15:59:00 +0000

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const http = require("http");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = "Hello Node!!\n";
   res.end(msg);
 });
 
